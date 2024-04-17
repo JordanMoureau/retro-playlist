2d73b75ea4fa3f70ca9d6b7e36c708f524a4b101
Jordan Moureau <jordanfromnovember@gmail.com>
2024-04-16 21:21:58 -0700

    searchbar functionality fixes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,14 +5,12 @@ export default function SearchBar({ searchCall }) {
 
   const handleSearch = useCallback((e) => {
     e.preventDefault();
-
     setSearch(e.target.value);
   }, []);
 
   const searched = useCallback(
     (e) => {
       e.preventDefault();
-
       searchCall(search);
       setSearch("");
     },
@@ -22,8 +20,14 @@ export default function SearchBar({ searchCall }) {
   return (
     <div className="search-bar">
       {/* <form onSubmit={handleSearch}> */}
-      <input placeholder="Search for songs" onChange={handleSearch} />
-      <button onClick={searched}>Submit</button>
+      <input
+        value={search}
+        placeholder="Search for songs"
+        onChange={handleSearch}
+      />
+      <button onClick={searched} disabled={search === "" ? "true" : ""}>
+        Submit
+      </button>
       {/* </form> */}
     </div>
   );
